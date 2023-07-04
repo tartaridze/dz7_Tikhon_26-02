fd16f52ff29638a58269e4b4b4be6a0f87ecc249
Тихон <35634024+tartaridze@users.noreply.github.com>
2023-07-04 18:19:24 +0600

    Update app.js

--- dz7_Tikhon_26--02/app.js
+++ dz7_Tikhon_26--02/app.js
@@ -12,11 +12,11 @@ const searchCity = () => {
             .then(data => {
                 city.innerHTML = data?.name ? data.name : 'Город не найден'
                 temp.innerHTML = data?.main ? Math.round(data.main.temp - 273) + '&deg' + 'C' : '..."'
-                if (cityName.value === '') {
-                        city.innerHTML = 'Введите город'
-                }
+            
             })
-
+        if (cityName.value === '') {
+            city.innerHTML = 'Введите город'
+        }
     })
 }
 
